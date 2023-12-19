6f4e565c68e0b1625f3c41f99a51dcd4c4b84e01
Andrew Leech <andrew.leech@planetinnovation.com.au>
2023-12-19 14:27:33 +1100

    build: install locked versions of dependencies.

--- build_msi.ps1
+++ build_msi.ps1
@@ -2,19 +2,20 @@
 wget https://github.com/mitsuhiko/rye/releases/download/0.11.0/rye-x86_64-windows.exe -UseBasicParsing -OutFile rye.exe
 $env:path += ";."
 
-rye sync
-rye run python -m __version__ --python --short --save
+rye run python -m ensurepip
+rye run python -m pip install -r requirements.lock
+rye run git-versioner --python --short --save
 
 # Install WIX Toolkit
 wget https://github.com/wixtoolset/wix3/releases/download/wix311rtm/wix311-binaries.zip -UseBasicParsing -OutFile c:\wix311-binaries.zip
 mkdir c:\\wix311
 Expand-Archive -Path c:\wix311-binaries.zip -DestinationPath c:\\wix311\\bin
-$env:WIX="C:\\wix311\\"
-$env:PYTHONIOENCODING="UTF-8"
+$env:WIX = "C:\\wix311\\"
+$env:PYTHONIOENCODING = "UTF-8"
 
 # Build app
 git status
-$version = @(rye run python -m __version__ --python --short)
+$version = @(rye run git-versioner --python --short)
 echo "Version: $version"
 .\pyoxidizer.exe build msi_installer --release --var version "$version"
 
